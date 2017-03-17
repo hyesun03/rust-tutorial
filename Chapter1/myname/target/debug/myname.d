52d4219c7630a996ed048bc3b1bc59cf5ed99cc6
hyesun03 <chsun0303@gmail.com>
2017-03-17 13:17:20 +0900

    Chapter1: Using cargo

---
+++ Chapter1/myname/target/debug/myname.d
@@ -0,0 +1,1 @@
+/Users/hsun/Documents/rust-tutorial/Chapter1/myname/target/debug/myname: /Users/hsun/Documents/rust-tutorial/Chapter1/myname/src/main.rs
---
+++ Chapter1/welcomec/target/debug/welcome.d
@@ -0,0 +1,1 @@
+/Users/hsun/Documents/rust-tutorial/Chapter1/welcomec/target/debug/welcome: /Users/hsun/Documents/rust-tutorial/Chapter1/welcomec/src/main.rs
